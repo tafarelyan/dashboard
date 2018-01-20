7726c12f7748dde272a658b0de6e705a934e6349
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-20 09:33:53 -0200

    Dict as parameter

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    data = [1, 2, 3, 4, 5]
-    return render_template('index.html', data=data)
+    payload = {
+        'type': 'line',
+        'data': {
+            'labels': ['One', 'Two', 'Three', 'Four', 'Five'],
+            'datasets': [{
+                'label': 'Test Line Chart',
+                'data': [1, 2, 3, 4, 5]
+            }],
+        },
+        'options': '',
+    }
+    return render_template('index.html', payload=payload)
 
 if __name__ == '__main__':
     app.run(debug=True)
